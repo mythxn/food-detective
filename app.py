0e7e0bd1060441e5a96c47151b5769e844536e9b
Mithun <mythxn@icloud.com>
2020-03-19 14:38:45 +0400

    app.py: Formatting

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sys
 import requests
 import random
 from bs4 import BeautifulSoup
-from googlesearch import search 
+from googlesearch import search
 from flask import Flask, redirect, request, url_for, render_template, session
 from datetime import datetime
 
@@ -23,9 +23,9 @@ def root():
     soup = BeautifulSoup(txt, 'html.parser')
 
     # scrape recipe of the day
-    rotd= soup.find('article',class_='fixed-recipe-card' )
-    f_name = rotd.find('span',class_='fixed-recipe-card__title-link').text
-    f_desc = rotd.find('div',class_='fixed-recipe-card__description').text
+    rotd = soup.find('article', class_='fixed-recipe-card')
+    f_name = rotd.find('span', class_='fixed-recipe-card__title-link').text
+    f_desc = rotd.find('div', class_='fixed-recipe-card__description').text
     f_link = rotd.a['href']
     f_img = rotd.a.img['data-original-src']
 
@@ -34,7 +34,8 @@ def root():
     cursor = connection.cursor()
     params = {'name': f_name, 'desc': f_desc, 'link': f_link, 'img': f_img}
     try:
-        cursor.execute("insert into recipeOfTheDay VALUES (:name, :desc, :link, :img)", params)
+        cursor.execute(
+            "insert into recipeOfTheDay VALUES (:name, :desc, :link, :img)", params)
     except:
         print("Already in DB; Not updating.")
     connection.commit()
@@ -42,76 +43,83 @@ def root():
 
     # google search for nearby restaurants
     query = "restaurants near me"
-    for j in search(query, tld="ae", num=1, stop=1, pause=2): 
+    for j in search(query, tld="ae", num=1, stop=1, pause=2):
         restaurant = j
-		
-    if (currentMonth >= 5 and currentMonth <10):
+
+    if (currentMonth >= 5 and currentMonth < 10):
         season = "Summer"
         query2 = "cold recipe for hot weather"
-		
+
     else:
         season = "Winter"
         query2 = "Winter recipe for cold weather"
 
-    for k in search(query2, tld="ae", num=1, stop=1, pause=2): 
+    for k in search(query2, tld="ae", num=1, stop=1, pause=2):
         recipe = k
 
     # display index page with recipe of the day
-	return render_template('index.html', f_name=f_name, f_desc=f_desc, f_link=f_link, f_img=f_img, restaurantnearme = restaurant, season = season, recipe = recipe)
+        return render_template('index.html', f_name=f_name, f_desc=f_desc, f_link=f_link, f_img=f_img, restaurantnearme=restaurant, season=season, recipe=recipe)
 
 # guestbook - display entries / as well as bs for 'weather'
 @app.route('/guestbook', methods=['POST', 'GET'])
 def gb():
     # establish connection with the weather website
-    page=requests.get("https://weather.com/en-IN/weather/tenday/l/af60f113ba123ce93774fed531be2e1e51a1666be5d6012f129cfa27bae1ee6c")
-    content=page.content
-    soup=BeautifulSoup(content,"html.parser")
-    l=[]
-    weather=""
-    all=soup.find("div",{"class":"locations-title ten-day-page-title"}).find("h1").text
-    
+    page = requests.get(
+        "https://weather.com/en-IN/weather/tenday/l/af60f113ba123ce93774fed531be2e1e51a1666be5d6012f129cfa27bae1ee6c")
+    content = page.content
+    soup = BeautifulSoup(content, "html.parser")
+    l = []
+    weather = ""
+    all = soup.find(
+        "div", {"class": "locations-title ten-day-page-title"}).find("h1").text
+
     # scrape the weather table
-    table=soup.find_all("table",{"class":"twc-table"})
+    table = soup.find_all("table", {"class": "twc-table"})
     for items in table:
         for i in range(len(items.find_all("tr"))-1):
             d = {}
             try:
-                d["date"]=items.find_all("span",{"class":"day-detail"})[i].text			
-                d["desc"]=items.find_all("td",{"class":"description"})[i].text
-                d["temp"]=items.find_all("td",{"class":"temp"})[i].text
-                d["wind"]=items.find_all("td",{"class":"wind"})[i].text
-                d["humidity"]=items.find_all("td",{"class":"humidity"})[i].text
+                d["date"] = items.find_all(
+                    "span", {"class": "day-detail"})[i].text
+                d["desc"] = items.find_all(
+                    "td", {"class": "description"})[i].text
+                d["temp"] = items.find_all("td", {"class": "temp"})[i].text
+                d["wind"] = items.find_all("td", {"class": "wind"})[i].text
+                d["humidity"] = items.find_all(
+                    "td", {"class": "humidity"})[i].text
 
                 # save into db
                 connection = sqlite3.connect(DB_FILE)
                 cursor = connection.cursor()
-                params = {'date': d["date"], 'desc': d["desc"], 'temp': d["temp"], 'wind': d["wind"], 'humidity': d["humidity"]}
+                params = {'date': d["date"], 'desc': d["desc"],
+                          'temp': d["temp"], 'wind': d["wind"], 'humidity': d["humidity"]}
                 try:
-                    cursor.execute("insert into weather VALUES (:date, :desc, :temp, :wind, :humidity)", params)
+                    cursor.execute(
+                        "insert into weather VALUES (:date, :desc, :temp, :wind, :humidity)", params)
                 except:
                     print("Already in DB; Not updating.")
                 connection.commit()
                 cursor.close()
 
             except:
-                d["date"]="None"
-                d["desc"]="None"
-                d["temp"]="None"
-                d["wind"]="None"
-                d["humidity"]="None"
+                d["date"] = "None"
+                d["desc"] = "None"
+                d["temp"] = "None"
+                d["wind"] = "None"
+                d["humidity"] = "None"
             l.append(d)
 
     # connect and scrape random qoute to show as quote of the day from brainyquote
     q_url = "https://www.brainyquote.com/topics/inspirational-quotes"
     response = requests.get(q_url)
-    soup = BeautifulSoup(response.content,'html.parser')
+    soup = BeautifulSoup(response.content, 'html.parser')
     quotes = soup.find_all('a', attrs={"title": "view quote"})
     quotesList = []
     for quote in quotes:
         quotesList.append(quote.text)
 
     # random quote
-    num = random.randint(0,len(quotesList))
+    num = random.randint(0, len(quotesList))
     qotd = quotesList[num]
 
     # save into db
@@ -128,21 +136,22 @@ def gb():
     # connect and scrape corona related data / what is corona?
     c_url = "https://www.who.int/news-room/q-a-detail/q-a-coronaviruses"
     response = requests.get(c_url)
-    soup = BeautifulSoup(response.content,'html.parser')
-    whatC = soup.find("div",{"class":"sf-accordion__content"}).find("p").text
+    soup = BeautifulSoup(response.content, 'html.parser')
+    whatC = soup.find("div", {"class": "sf-accordion__content"}).find("p").text
 
     # connect and scrape corona related data / corona symptoms
     c_url = "https://www.who.int/health-topics/coronavirus"
     response = requests.get(c_url)
-    soup = BeautifulSoup(response.content,'html.parser')
-    symptomsC = soup.find_all('span', attrs={"style": "background-color:transparent;text-align:inherit;text-transform:inherit;white-space:inherit;word-spacing:normal;caret-color:auto;"})[0].text
+    soup = BeautifulSoup(response.content, 'html.parser')
+    symptomsC = soup.find_all('span', attrs={
+                              "style": "background-color:transparent;text-align:inherit;text-transform:inherit;white-space:inherit;word-spacing:normal;caret-color:auto;"})[0].text
 
     # connect and scrape corona related data / who to contact?
     c_url = "https://www.dha.gov.ae/Covid19/Pages/home.aspx"
     response = requests.get(c_url)
-    soup = BeautifulSoup(response.content,'html.parser')
-    contactC = soup.find_all('ul', attrs={"style": "list-style-type:disc;margin-left:22.15px;"})[1].text
-
+    soup = BeautifulSoup(response.content, 'html.parser')
+    contactC = soup.find_all(
+        'ul', attrs={"style": "list-style-type:disc;margin-left:22.15px;"})[1].text
 
     try:
         connection = sqlite3.connect(DB_FILE)
@@ -160,7 +169,8 @@ def _insert(name, email, comment):
     params = {'name': name, 'email': email, 'comment': comment}
     connection = sqlite3.connect(DB_FILE)
     cursor = connection.cursor()
-    cursor.execute("insert into guestbook VALUES (:name, :email, :comment)", params)
+    cursor.execute(
+        "insert into guestbook VALUES (:name, :email, :comment)", params)
     connection.commit()
     cursor.close()
 
@@ -168,7 +178,8 @@ def _insert(name, email, comment):
 # guestbook - insert entries / button action
 @app.route('/sign', methods=['POST'])
 def sign():
-    _insert(request.form['name'], request.form['email'], request.form['comment'])
+    _insert(request.form['name'], request.form['email'],
+            request.form['comment'])
     return redirect(url_for('gb'))
 
 
@@ -183,7 +194,8 @@ def _newaccount(username, email, password):
     params = {'username': username, 'email': email, 'password': password}
     connection = sqlite3.connect(DB_FILE)
     cursor = connection.cursor()
-    cursor.execute("insert into accounts VALUES (:username, :email, :password)", params)
+    cursor.execute(
+        "insert into accounts VALUES (:username, :email, :password)", params)
     connection.commit()
     cursor.close()
 
@@ -191,7 +203,8 @@ def _newaccount(username, email, password):
 # register - insert entries / button action
 @app.route('/register', methods=['POST'])
 def register():
-    _newaccount(request.form['username'], request.form['email'], request.form['password'])
+    _newaccount(request.form['username'],
+                request.form['email'], request.form['password'])
     return redirect(url_for('login'))
 
 
@@ -205,7 +218,8 @@ def login():
 @app.route('/signin', methods=['POST', 'GET'])
 def signin():
     if request.method == 'POST':
-        query = "select * from accounts where username = '" + request.form['username']
+        query = "select * from accounts where username = '" + \
+            request.form['username']
         query = query + "' and password = '" + request.form['password'] + "';"
         connection = sqlite3.connect(DB_FILE)
         cur = connection.execute(query)
